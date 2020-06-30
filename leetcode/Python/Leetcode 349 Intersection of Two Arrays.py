fb948aa4a288b1eeb61bd256b1745f6ec217a513
Allen Chang <achang35@gmail.com>
2020-06-30 01:12:08 +0000

    Add descriptions for fellows 349 / 53

--- leetcode/Python/Leetcode 349 Intersection of Two Arrays.py
+++ leetcode/Python/Leetcode 349 Intersection of Two Arrays.py
@@ -6,16 +6,23 @@ class Solution(object):
         :rtype: List[int]
         """
         
-       # sort and use pointers
-       # store in hash map for constant look up
-       # 
-    
+       
+        # There are multiple ways to solve this problem. I will show one approach using pointers and a dictionary. I encourage you to think about other ways to solve the problem!
+        
+        # Looking at the inputs, I need to keep in mind two things. 
+        # 1) The elements are out of order. If I'm searching for matches between arrays, it's inefficient to have to check each array. One way to solve this challenge is to sort both arrays in place before iterating through them. 
+        # 2) The second thing to keep in mind is that integers can be repeated within an array and I don't want to double count that in my array.
+        
         nums1.sort()
         nums2.sort()
+        
+        # Here I set up two pointers to keep track of where I am in each array. I also set up a dictionary to solve my second problem. Remember, a dictionary has constant look up time. 
         
         a = 0
         b = 0
         res = {}
+        
+        # Next I iterate through each array, if I ever reach the end of the array, I know there's no more elements to check for intersection. As I iterate, I check the values at each of my pointers. If one is greater than the other, I move the pointer until I find when the array values are equal. From there, I check if the value is in my dictionary. If not, add it. Ohterwise move both pointers forward.
         
         while a < len(nums1) and b < len(nums2):
             if nums1[a] < nums2[b]:
@@ -29,4 +36,6 @@ class Solution(object):
                 b += 1
                 
         return res
-                
+    
+        # Time complexity: Sorting in place is O(n log n) time and the loop is O(n), where n is the length of the shorter array. 
+        # Space complexity: O(n) where there is perfect intersection across arrays and the values within each array are unique.
--- leetcode/Python/Leetcode 53 Maximum Subarray.py
+++ leetcode/Python/Leetcode 53 Maximum Subarray.py
@@ -5,21 +5,22 @@ class Solution(object):
         :rtype: int
         """
         
-        # make a window
-        # calculate the window sum
-        # check max of window_sum + curr or curr
-        # keep track of window sum
-        # keep track of max sum
-        # return window sum at end
+        # I'll start by solving this using the linear approach. 
+        # One way to think about this problem is to use expanding windows. At each index, I ask if it makes sense to expand or reset the window. The decision criteria I use is whether the sum of the current value and the previous window is greater than the current value. Remember that the questions asks for the CONTIGUOUS array, so even if I'm adding a negative number, if the sum + current value > current value, I should expand the window. 
+        
+        # If you're struggling with the logic, it might be helpful to go back and think about the stock selling problem again. 
         
         window_sum = nums[0]
-        n = len(nums)
         max_sum = window_sum
+        n = len(nums)
         
-        
-        for i in range(1, n):
-            curr = nums[i]
-            window_sum = max(window_sum + curr, curr)
+        for i in range(1,n):
+            curr_value = nums[i]
+            window_sum = max(window_sum + curr_value, curr_value)
+            
             max_sum = max(max_sum, window_sum)
         
         return max_sum
+    
+        # Time complexity: O(n) because I walk through each element of the array (minus the first)
+        # Space complexity: O(1) constant because I am updating a single variable continuously.
